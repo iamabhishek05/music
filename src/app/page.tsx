142fc84f06ab336c0505738c57e06c36054ca34d
Abhishek Tiwari <abhishektiwari290699@gmail.com>
2024-03-25 00:14:06 +0530

    partial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import FeaturedCourses from '@/components/FeaturedCourses'
 import HeroSection from '@/components/HeroSection'
+import WhyChooseUs from '@/components/WhyChooseUs'
+
 
 
 const page = () => {
     return (
         <main>
             <HeroSection />
+            <FeaturedCourses />
+            <WhyChooseUs />
         </main>
     )
 }
